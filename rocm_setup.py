fd97b61b3c8a7ba0440650220e26cafe0b97634a
felipe-ra <felipeangel50@gmail.com>
2024-05-12 12:43:23 -0500

    fixed typo on rocm setup instructions

--- rocm_setup.py
+++ rocm_setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name="GPU Info",
+    name="AMD GPU Info",
     ext_modules=cythonize("rocm_smi_gpu_power.pyx"),
     include_dirs=["/opt/rocm/include"],
     library_dirs=["/opt/rocm/lib"],
-    libraries=["rocm_smi64"]  # change if your installation is different 
 )
